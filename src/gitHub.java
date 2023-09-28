a58730301e27ccc309f7bb7eb9a1ddf0c3a79b20
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2023-09-28 12:05:06 +0200

    yenilik

--- src/gitHub.java
+++ src/gitHub.java
@@ -1,6 +1,7 @@
 public class gitHub {
     public static void main(String[] args) {
         System.out.println("ilk GitHub uygulamam");
+        System.out.println("yeni bir degisiklik yapiyorum");
     }
 
 }
