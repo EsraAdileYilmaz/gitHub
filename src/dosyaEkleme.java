052aff64a1522c6537cb7d9b1b4db36587522ab5
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2023-09-28 16:16:57 +0200

    dosyaEkleme

---
+++ src/dosyaEkleme.java
@@ -0,0 +1,7 @@
+public class dosyaEkleme {
+    public static void main(String[] args) {
+
+        System.out.println("git hub'da dosya ekleme");
+        System.out.println("yeni bir class ekledim");
+    }
+}
